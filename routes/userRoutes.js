deda1252f1c5125591acaa7c8eb13c8fbe7418eb
Sanjay .S <161437341+SanjayShanmugam21@users.noreply.github.com>
2025-05-31 13:14:46 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const verifyToken=require("../middleware/authMiddleware");
 const authorizeRole=require("../middleware/roleMiddleware")
-// Only admin
+// Only admin access
 router.get('/admin',verifyToken, authorizeRole("admin"),(req, res) => {
   res.json({ message: "welcome admin" });
 });
 
-// Both access (admin and manager maybe)
+// Both access (admin and manager )
 router.get('/manager',verifyToken, authorizeRole("admin","manager"),(req, res) => {
   res.json({ message: "welcome manager" });
 });
 
-// All can access
+// All can access (admin and manager ,user)
 router.get('/user',verifyToken,authorizeRole("admin","manager","user"), (req, res) => {
   res.json({ message: "welcome user" });
 });
